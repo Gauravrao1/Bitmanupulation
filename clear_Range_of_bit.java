bb26079ae9ec5a65399bc1438c4d78272e74423b
Gaurav Rao <gauravraorao6@gmail.com>
2025-09-06 16:55:14 +0530

    clear bit or make zero in given range

---
+++ clear_Range_of_bit.java
@@ -0,0 +1,14 @@
+public class clear_Range_of_bit {
+
+        public static int clearibit(int n,int i,int j){
+                int a =((~0)<<(j+1));
+                int b = (1<<i)-1;
+                int bitmask =a|b;
+                return n & bitmask;
+        }
+         
+        public static void main(String[] args) {
+              System.out.println( clearibit(10, 2, 4)); 
+                
+        }
+}
